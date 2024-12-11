29e83fb921699e45e790ee53378482b02b30451c
rakib544 <md.rakib10122003@gmail.com>
2024-12-11 12:43:25 +0600

    feat: add authentication and authorization layer on tutorial api endpoints

--- src/modules/tutorials/tutorials.route.ts
+++ src/modules/tutorials/tutorials.route.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import TutorialController from './tutorials.controller';
 
 import { CreateTutorialDto, UpdateTutorialDto } from '@/dto/tutorial.dto';
+import { verifyAdmin, verifyAuthToken } from '@/middlewares/auth';
 import RequestValidator from '@/middlewares/request-validator';
 
 const tutorial: Router = Router();
@@ -10,18 +11,24 @@ const tutorialController = new TutorialController();
 
 tutorial.post(
   '/',
+  verifyAuthToken,
+  verifyAdmin,
   RequestValidator.validate(CreateTutorialDto),
   tutorialController.addTutorial
 );
-tutorial.get('/', tutorialController.getTutorials);
+tutorial.get('/', verifyAuthToken, tutorialController.getTutorials);
 tutorial.put(
   '/:id',
+  verifyAuthToken,
+  verifyAdmin,
   RequestValidator.isValidObjectId(),
   RequestValidator.validate(UpdateTutorialDto),
   tutorialController.updateTutorial
 );
 tutorial.delete(
   '/:id',
+  verifyAuthToken,
+  verifyAdmin,
   RequestValidator.isValidObjectId(),
   tutorialController.deleteTutorial
 );
