bd5cdf3783d3e74776e95f68b27e2d02049cb869
rakib544 <md.rakib10122003@gmail.com>
2024-12-11 12:45:40 +0600

    feat: add authentication and authorization layer in lessons api endpoint

--- src/modules/lessons/lessons.route.ts
+++ src/modules/lessons/lessons.route.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import LessonController from './lessons.controller';
 
 import { CreateLessonDto, UpdateLessonDto } from '@/dto/lesson.dto';
+import { verifyAdmin, verifyAuthToken } from '@/middlewares/auth';
 import RequestValidator from '@/middlewares/request-validator';
 
 const lesson: Router = Router();
@@ -10,19 +11,25 @@ const lessonController = new LessonController();
 
 lesson.post(
   '/',
+  verifyAuthToken,
+  verifyAdmin,
   RequestValidator.validate(CreateLessonDto),
   lessonController.createLesson
 );
-lesson.get('/', lessonController.getLessons);
-lesson.get('/:lessonNo', lessonController.getLesson);
+lesson.get('/', verifyAuthToken, lessonController.getLessons);
+lesson.get('/:lessonNo', verifyAuthToken, lessonController.getLesson);
 lesson.put(
   '/:id',
+  verifyAuthToken,
+  verifyAdmin,
   RequestValidator.isValidObjectId(),
   RequestValidator.validate(UpdateLessonDto),
   lessonController.updateLesson
 );
 lesson.delete(
   '/:id',
+  verifyAuthToken,
+  verifyAdmin,
   RequestValidator.isValidObjectId(),
   lessonController.deleteLesson
 );
